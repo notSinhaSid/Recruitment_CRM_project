3333c1f780226fecb5249bdab943d0348cd65287
notSinhaSid <deadlysinhasiddhartha2000@gmail.com>
2026-07-26 22:17:06 +0530

    added slug on boot, and role seeder data

--- recrivo/app/Models/Tenant.php
+++ recrivo/app/Models/Tenant.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
 
 class Tenant extends Model
 {
@@ -14,6 +15,14 @@ class Tenant extends Model
         'name',
         'slug',
     ];
+
+    protected static function booted(): void {
+        static::creating(function($tenant) {
+            if(empty($tenant->slug)){
+                $tenant->slug = Str::slug($tenant->name);
+            }
+        });
+    }
 
     public function users(): HasMany{
         return $this->hasMany(User::class);
